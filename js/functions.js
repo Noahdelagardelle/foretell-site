47f6e2cfbcf5dd8af0f4644cecf24ed9e8f9ed9a
Noah Delagardelle <noahcdela@gmail.com>
2018-04-25 16:40:29 -0500

    Add projects and general updates

--- js/functions.js
+++ js/functions.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 
-
+    // Hero question popups
     $('.animation-lines-container').hover(
         function () {
             $(this).find('.question-container').addClass('hovered');
@@ -10,19 +10,45 @@ $(document).ready(function () {
         }
     );
 
+    // Project grid thumbnails
+    $('.project-card').hover(
+        function () {
+            $(this).find('.project-logo').addClass('project-card-hovered color-logo');
+            $(this).find('.project-card-name').addClass('name-reveal');
+        },
+        function () {
+            $(this).find('.project-logo').removeClass('project-card-hovered color-logo');
+            $(this).find('.project-card-name').removeClass('name-reveal');
+        }
+    );
+
     $('.project-info-container').removeClass('fade--in');
     setTimeout(function () {
         $('.project-navigation').removeClass('fade--in');
     }, 600);
 
-    // $('.letter-f > .animate-in-mask').removeClass('slide--right');
-
     $('.animation-lines-container .animate-in-mask').each(function (i) {
         setTimeout(function () {
             $('.animation-lines-container .animate-in-mask').eq(i).addClass('is-visible');
-        }, 300 * i);
+        }, 100 * i);
     });
 
+    setTimeout(function () {
+        $('.animation-lines-container .question').each(function (i) {
+            setTimeout(function () {
+                $('.animation-lines-container .question').eq(i).addClass('question-reveal');
+            }, 300 * i);
+            setTimeout(function () {
+                $('.animation-lines-container:nth-child(1)').eq(i).addClass('pink');
+            }, 400 * i);
+        });
+    }, 3000);
 
-
+    // $('.main-carousel').flickity({
+    //     cellAlign: 'left',
+    //     contain: true,
+    //     wrapAround: true,
+    //     fullscreen: true,
+    //     lazyLoad: 1
+    // });
 });
